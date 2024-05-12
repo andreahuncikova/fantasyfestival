4c8e83a6e60ea52b4d2a4377aec7f3449724aa01
andreahuncikova <andhun01@easv365.dk>
2024-05-13 01:02:45 +0200

    Add new feature

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from '@/components/Navbar.vue';
+
 </script>
 
 <template>
       <Navbar />
-
+      <transition name="fade" mode="out-in">
   <RouterView />
+</transition>
 </template>
 
 <style scoped>
-
+/* CSS for fade animation */
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 5s
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-
+    <div class="hero">
+      <video autoplay loop muted plays-inline>
+        <source src="@/assets/videjko.mp4" type="video/mp4">
+      </video>
+    </div>
   </div>
 </template>
 
@@ -9,5 +13,9 @@
 </script>
 
 <style lang="scss" scoped>
-
+  .hero {
+    width: 100%;
+    max-height: 100vh;
+    position: relative;
+  }
 </style>
