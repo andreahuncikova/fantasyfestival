c64e3dc828af5b06b821be0025f7a7e17ec07944
andreahuncikova <andhun01@easv365.dk>
2024-05-12 23:50:32 +0200

    Navbar

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,28 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/festival',
+      name: 'festival',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/FestivalView.vue')
+    },
+    {
+      path: '/program',
+      name: 'program',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProgramView.vue')
+    },
+    {
+      path: '/transport',
+      name: 'transport',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/TransportView.vue')
     }
   ]
 })
